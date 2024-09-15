24282379ca2f0fca55aa9635afb888f1852c27b7
Lemoan-01 <133101491+Lemoan-01@users.noreply.github.com>
2024-09-15 14:11:15 +0200

    jej
    anoueghrt

---
+++ CampingApp3/CampingApp3/ViewModels/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using CampingApp3.Views.UserControls;
+using CampingApp3.ViewModels;
+using System.Windows.Input;
+
+namespace Homepage.ViewModels
+{
+    class LoginVM : ViewModelBase
+    {
+        public ICommand NavigateToRegisterCommand { get; }
+
+        public LoginVM()
+        {
+            NavigateToRegisterCommand = new RelayCommand(NavigateToRegister);
+        }
+
+        private void NavigateToRegister(object obj)
+        {
+            var navigationVM = App.Current.MainWindow.DataContext as NavigationVM;
+            navigationVM.CurrentView = new RegisterVM();
+        }
+    }
+}
